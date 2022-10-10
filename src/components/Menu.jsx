2621b740f6918f95a524740af2ce35daf8593acd
luiznaiper <luiz_naiper@hotmail.com>
2022-10-10 18:56:35 -0500

    :construction: Add NavLink and Link components

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { routes } from '../helpers/routes';
 
 const Menu = () => {
-  return <nav>Menu</nav>;
+  return (
+    <nav>
+      <ul>
+        {routes.map((route) => (
+          <li key={route.id}>
+            <NavLink
+              style={({ isActive }) => ({
+                color: isActive ? 'red' : 'blue',
+              })}
+              to={route.path}
+              end
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Menu;
---
+++ src/helpers/routes.js
@@ -0,0 +1,19 @@
+const routes = [
+  {
+    id: 1,
+    path: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    path: '/blog',
+    text: 'Blog',
+  },
+  {
+    id: 3,
+    path: '/profile',
+    text: 'Profile',
+  },
+];
+
+export { routes };
