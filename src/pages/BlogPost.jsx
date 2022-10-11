d509a35f489a6fa18eee845a0e7b2e5e79c2fe8d
luiznaiper <luiz_naiper@hotmail.com>
2022-10-10 19:33:18 -0500

    :construction: Add useNavigate() hook to create a button to go back

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import blogdata from '../helpers/blogdata';
 
 const BlogPost = () => {
+  const navigate = useNavigate();
   const { slug } = useParams();
   const blogpost = blogdata.find((post) => post.slug === slug);
+  const returnToBlog = () => {
+    navigate(-1);
+  };
+
   return (
     <>
       <h2>{blogpost.title}</h2>
+      <button onClick={returnToBlog}>Back to blog</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
     </>
